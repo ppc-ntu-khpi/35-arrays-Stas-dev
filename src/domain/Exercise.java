fa7874a2af78cf0b8996a0b0eec4bbebfecadefc
Stanislav Utkin <stasytkin0000@gmail.com>
2021-02-09 20:56:36 +0200

    +code

--- src/domain/Exercise.java
+++ src/domain/Exercise.java
@@ -1,7 +1,45 @@
 package domain;
+import java.util.ArrayList;
 
-public class Exercise {
-    public static float Calculate(int R){
-        return (float) (2*Math.PI*Math.sqrt(R));
+
+
+
+public  class Exercise {
+   public  static int Calculate (){
+        int maxValue = 999999;
+         ArrayList<Integer> happyTickets = new ArrayList<>();
+         
+         foo(maxValue,  happyTickets);
+      
+         int quontity  = happyTickets.size();
+   return quontity;
+   }
+    
+  
+    
+    
+    
+    /**
+     * якщо сума цифр рівна - білет щасливий;
+     * @param maxValue  - максимальне значення квитку 
+     */
+    
+    public static void foo (int maxValue, ArrayList<Integer> happyTickets){
+         
+        for(int i = 1; i<maxValue; i++){
+                if(i%10 + i%100 + i%100 == i%1000+i%10000+i%100000){
+                
+                    happyTickets.add(i);
+                
+                }
+        
+        }     
+    
     }
+    
+   
+    
+
+
+
 }
--- src/test/TestResult.java
+++ src/test/TestResult.java
@@ -4,9 +4,9 @@ import domain.Exercise;
 
 public class TestResult {
 
-    public static void main(String[] args) {
+    public static  void main(String[] args) {
 
-        int R=10;
-        System.out.println("Area of a circle for R="+R+"m is "+Exercise.Calculate(R)+"m"+'\u00B2');
+       
+        System.out.println("Кількість щасливих квитків серед 999999: \t" + Exercise.Calculate());
     }
 }
